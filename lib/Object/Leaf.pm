9bf725ed2a5735d9bf9884c8baf4463f62632ead
jeffa <jeffa@unlocalhost.com>
2013-05-24 19:00:02 -0400

    init

---
+++ lib/Object/Leaf.pm
@@ -0,0 +1,83 @@
+package Object::Leaf;
+use Moose::Role;
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Object::Leaf - Represents and defines behavior for primitive objects in the
+composition. A leaf has no children (e.g. Rectangle, Line, Text, etc.).
+
+=head1 SYNOPSIS
+
+Object::Leaf is a role to be consumed by an object.
+
+  package Line;
+  use Pattern::Composite;
+
+  with 'Object::Leaf'
+
+  sub draw {
+    my ($self) = @_;
+
+    # magic!
+  } 
+
+  1;
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Pattern::Composite>
+
+=item L<Object::Composite>
+
+=item L<Object::Component>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Jeffrey Hayes Anderson.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=cut
+
+1;
+use Moose;
+our $VERSION = '0.01';
+
