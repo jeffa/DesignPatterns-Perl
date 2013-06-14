ffe725e535754c185e2704e014641a82d4988598
jeffa <jeffa@unlocalhost.com>
2013-06-14 18:25:33 -0400

    removed cruft

--- t/lib/Test/Composite/Cabinet.pm
+++ t/lib/Test/Composite/Cabinet.pm
@@ -6,7 +6,4 @@ has '+power'          => ( default => 1 );
 has '+net_price'      => ( default => 50 );
 has '+discount_price' => ( default => 40 );
 
-# TODO: replace children with OODP::Iterator
-#has children => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
-
 1;
