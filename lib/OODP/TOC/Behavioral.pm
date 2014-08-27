9a2912d12166b83c1541a4cf94a30fc367ed10bb
jeffa <jeffa@unlocalhost.com>
2014-08-26 17:38:59 -0700

    moved all TOC modules into TOC/

---
+++ lib/OODP/TOC/Behavioral.pm
@@ -0,0 +1,151 @@
+package OODP::TOC::Behavioral;
+our $VERSION = '0.01';
+
+=head1 NAME
+
+OODP::TOC::Behavioral - Table of Contents for Behavioral Design Patterns
+
+=head1 Chain Of Responsibility
+
+=over 4
+
+=item L<OODP::TOC::ChainOfResponsibility>
+
+=back
+
+=head1 Command
+
+=over 4
+
+=item L<OODP::TOC::Command>
+
+=back
+
+=head1 Interpreter
+
+=over 4
+
+=item L<OODP::TOC::Interpreter>
+
+=back
+
+=head1 Iterator
+
+=over 4
+
+=item L<OODP::TOC::Iterator>
+
+=back
+
+=head1 Mediator
+
+=over 4
+
+=item L<OODP::TOC::Mediator>
+
+=back
+
+=head1 Memento
+
+=over 4
+
+=item L<OODP::TOC::Memento>
+
+=back
+
+=head1 Observer
+
+=over 4
+
+=item L<OODP::TOC::Observer>
+
+=back
+
+=head1 State
+
+=over 4
+
+=item L<OODP::TOC::State>
+
+=back
+
+=head1 Strategy
+
+=over 4
+
+=item L<OODP::TOC::Strategy>
+
+=back
+
+=head1 Template Method
+
+=over 4
+
+=item L<OODP::TOC::TemplateMethod>
+
+=back
+
+=head1 Vistor
+
+=over 4
+
+=item L<OODP::TOC::Visitor>
+
+=back
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<OODP::TOC::Creational>
+
+=item L<OODP::TOC::Structural>
+
+=back
+
+=head1 AUTHOR
+
+Jeffrey Hayes Anderson, C<< <info+cpan at unlocalhost.com> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Jeffrey Hayes Anderson.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=cut
+
+1;
