2f64ac8ab186fdb5f1df28d55a35f2669ffd1278
jeffa <jeffa@unlocalhost.com>
2015-05-06 13:18:46 -0400

    need placeholder for Makefile

---
+++ lib/OODP.pm
@@ -0,0 +1,15 @@
+package OODP;
+our $VERSION = '0.01';
+1;
+__END__
+=head1 NAME
+
+OODP - placeholder for project version and documentation start.
+
+=head1 SYNOPSIS
+
+Perl (with Moose) implementations of the Gang of Four Design Patterns.
+
+=head1 LICENSE AND COPYRIGHT
+
+See L<License.md>
